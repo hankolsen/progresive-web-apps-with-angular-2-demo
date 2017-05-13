50bca105409578cc2150021d57d5daa48d4aaefe
hank <henrik@hankolsen.com>
2017-05-13 13:19:59 +0200

    Updated npm dependencies

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,26 +2,23 @@ import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {HttpModule} from "@angular/http";
 import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {BookingStatusPipe} from "./booking-status.pipe";
 import {FlightSearchComponent} from "./flightsearch.component";
-import {MdCardModule} from "@angular2-material/card";
-import {MdButtonModule} from "@angular2-material/button";
-import {MdIconModule} from "@angular2-material/icon";
-import {MdToolbarModule} from "@angular2-material/toolbar";
-import {MdSidenavModule} from "@angular2-material/sidenav";
-import {MdListModule} from "@angular2-material/list";
+import {MdCardModule, MdButtonModule, MdIconModule, MdToolbarModule, MdSidenavModule, MdListModule} from "@angular/material";
 
 @NgModule({
     imports: [
+        BrowserAnimationsModule,
         BrowserModule,
         HttpModule,
         FormsModule,
-        MdButtonModule.forRoot(),
-        MdCardModule.forRoot(),
-        MdIconModule.forRoot(),
-        MdListModule.forRoot(),
-        MdSidenavModule.forRoot(),
-        MdToolbarModule.forRoot()
+        MdButtonModule,
+        MdCardModule,
+        MdIconModule,
+        MdListModule,
+        MdSidenavModule,
+        MdToolbarModule
     ],
     declarations: [
         BookingStatusPipe, FlightSearchComponent
@@ -34,4 +31,3 @@ import {MdListModule} from "@angular2-material/list";
 })
 export class AppModule {
 }
-
