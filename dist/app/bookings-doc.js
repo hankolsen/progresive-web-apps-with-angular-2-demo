e3d2034a2a9278c6b15c2edd28572bc908d3241b
hankolsen <henrik@hankolsen.com>
2017-05-13 13:33:10 +0200

    Updated .gitignore

--- dist/app/bookings-doc.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var BookingsDoc = (function () {
-    function BookingsDoc() {
-    }
-    return BookingsDoc;
-}());
-exports.BookingsDoc = BookingsDoc;
-//# sourceMappingURL=bookings-doc.js.map
